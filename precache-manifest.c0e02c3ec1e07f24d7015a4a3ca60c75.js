e2098db351c2556bed567c1fc55cb21b2e5bc16f
Cosmo Wang <yw259@cs.washington.edu>
2020-08-31 15:37:54 -0700

    Updates

---
+++ precache-manifest.c0e02c3ec1e07f24d7015a4a3ca60c75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1cf7d19442599606c5e413ddaafcd5",
+    "url": "/bangumi-ratings/index.html"
+  },
+  {
+    "revision": "203e9a5bf1de18d4fa13",
+    "url": "/bangumi-ratings/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4d43b9ac694508da10e5",
+    "url": "/bangumi-ratings/static/css/main.fee145d6.chunk.css"
+  },
+  {
+    "revision": "203e9a5bf1de18d4fa13",
+    "url": "/bangumi-ratings/static/js/2.0126ccd4.chunk.js"
+  },
+  {
+    "revision": "668304d60e4832618425ffc200830b0a",
+    "url": "/bangumi-ratings/static/js/2.0126ccd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d43b9ac694508da10e5",
+    "url": "/bangumi-ratings/static/js/main.23d0f891.chunk.js"
+  },
+  {
+    "revision": "b0b45692684939526f38",
+    "url": "/bangumi-ratings/static/js/runtime-main.bd71931c.js"
+  },
+  {
+    "revision": "bed605a9f89a9e8fbee3b4a90eebf1f2",
+    "url": "/bangumi-ratings/static/media/拜年祭2020.bed605a9.jpg"
+  }
+]);
