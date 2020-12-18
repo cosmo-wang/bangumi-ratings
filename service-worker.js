287aa9867f4c0c10d48aaa53b01798e0eead7c01
Cosmo Wang <yw259@cs.washington.edu>
2020-12-17 19:10:08 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bangumi-ratings/precache-manifest.23a603b7e06b7931370235dc8954f022.js"
+  "/bangumi-ratings/precache-manifest.d56442f7c27f8ba7834029130969dcaf.js"
 );
 
 self.addEventListener('message', (event) => {
