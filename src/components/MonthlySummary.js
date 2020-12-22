415095027802cb12d28413b81179c040a21c0078
Cosmo Wang <yw259@cs.washington.edu>
2020-12-22 12:44:59 -0800

    finished monthly summary

--- src/components/MonthlySummary.js
+++ src/components/MonthlySummary.js
@@ -7,19 +7,35 @@ import '../App.css';
 
 function Summary(props) {
   return <div className="summary">
-    <div className="month">{moment(props.month).format("YYYY[年]MM[月]")}</div>
-    <div className="bangumi-num">{"番剧总数：" + props.summary.bangumi_num}</div>
-    <div>{"总集数：" + props.summary.tv_episode_num}</div>
-    <div>{"总时长：" + props.summary.total_time}</div>
-    <div>{"每日时长：" + formatTime(props.summary.total_time / moment(props.month).daysInMonth())}</div>
-    <div>{"番剧列表：" + props.summary.bangumis.join(", ")}</div>
+    <div className="summary-title">
+      <div className="summary-info">
+        <div className="year-month">
+          <div className="month">{moment(props.month).format("MM")}</div>
+          <div className="year">{moment(props.month).format("YYYY")}</div>
+          <div className="yue">月</div>
+        </div>
+        <div className="summary-info-pieces">
+          <div className="summary-info-piece">{"番剧总数：" + props.summary.bangumi_num}</div>
+          <div className="summary-info-piece">{"总集数：" + props.summary.tv_episode_num}</div>
+          <div className="summary-info-piece">{"总时长：" + props.summary.total_time}</div>
+          <div className="summary-info-piece">{"每日时长：" + formatTime(props.summary.total_time / moment(props.month).daysInMonth())}</div>
+        </div>
+      </div>
+      <div className="summary-cover">
+
+      </div>
+    </div>
+    <div className="summary-names">
+      {props.summary.bangumis.map((bangumi_name) => 
+        <div className="bangumi-name">{"• " + bangumi_name}</div>)}
+    </div>
   </div>
 }
 
 function MonthlySummary() {
   const { summaries } = React.useContext(AnimeDataContext);
   console.log(summaries);
-  return <div className="main-element monthly-summary">
+  return <div className="monthly-summary">
     {Object.keys(summaries).map((month) => <Summary month={month} summary={summaries[month]} />)}
   </div>;
 }
